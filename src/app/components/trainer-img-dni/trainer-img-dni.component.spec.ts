1183627f6f9074e6fda80e6dd9c823ecac091364
Jonatan <intartif@gmail.com>
2021-02-12 10:09:32 -0500

    Fix build failed

--- src/app/components/trainer-img-dni/trainer-img-dni.component.spec.ts
+++ src/app/components/trainer-img-dni/trainer-img-dni.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TrainerImgDniComponent } from './trainer-img-dni.component';
 
@@ -6,7 +6,7 @@ describe('TrainerImgDniComponent', () => {
   let component: TrainerImgDniComponent;
   let fixture: ComponentFixture<TrainerImgDniComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TrainerImgDniComponent ]
     })
